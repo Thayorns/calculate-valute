312c7d729e39ad7ab43619d35eb4b9bb573a5544
valentine <thayornswordsman@gmail.com>
2024-04-27 14:41:43 +0300

    first commit

--- task_1/index.js
+++ task_1/index.js
@@ -1,5 +1,29 @@
 import {encoded, translations} from './data.js'
+function decodeFields(encoded, translations) {
+    const decoded = [];
 
+    const uniqueIds = new Set();
+
+    encoded.forEach(item => {
+        const decodedItem = {...item};
+
+        for (const key in item) {
+            if (key.endsWith("Id") && translations[item[key]]) {
+                decodedItem[key] = translations[item[key]];
+                uniqueIds.add(item[key]);
+            }
+        }
+
+        decoded.push(decodedItem);
+    });
+
+    console.log("Уникальные id из encoded:", Array.from(uniqueIds));
+
+    return decoded;
+}
+
+const decodedData = decodeFields(encoded, translations);
+console.log(decodedData);
 console.log("Let's rock")
 console.log(encoded, translations)
 
--- task_2/src/lib/Counter.svelte
+++ task_2/src/lib/Counter.svelte
@@ -1,10 +1,77 @@
+
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
+  getValutes()
+  
+  const rates = {
+    USD: 0,
+    RUB: 0
   }
+
+  async function getValutes() {
+    const res = await fetch('https://open.er-api.com/v6/latest/USD')
+    const data = await res.json()
+
+    rates.USD = data.rates.USD
+    rates.RUB = data.rates.RUB
+    console.log(rates)
+  }
+  
+  let finances = [
+		{
+			id: 1,
+			text: `USD`
+		},
+		{
+			id: 2,
+			text: `RUB`
+		}
+	]
+  
+  // let count = rates.USD
+  // $: calc = count * rates.RUB
+	
+  let selected1 = finances[0], 
+    selected2 = finances[1]
+  let input1:number, 
+    input2:number
+  
+  function calculate(source){
+    let rate1 = rates[selected2.text]
+    let rate2 = rates[selected1.text]
+    if(source === 'input1') {
+      input2 = (input1 * rate1) / rate2
+    }else {input1 = (input2 * rate2) / rate1}
+  }
+  
 </script>
 
-<button on:click={increment}>
-  count is {count}
-</button>
+
+	<select
+    bind:value={selected1}
+    on:change={() => (input1 = undefined)}
+	>
+		{#each finances as finance}
+			<option value={finance}>
+				{finance.text}
+			</option>
+		{/each}
+	</select>
+  <input type='number' bind:value={input1} on:input={() => calculate('input1')}>
+  <span></span>
+
+  <br/>
+
+	<select
+		bind:value={selected2}
+    on:change={() => (input2 = undefined)}
+	>
+		{#each finances as finance}
+			<option value={finance}>
+				{finance.text}
+			</option>
+		{/each}
+	</select>
+  <input type='number' bind:value={input2} on:input={() => calculate('input2')}> 
+  <span></span>
+
+
